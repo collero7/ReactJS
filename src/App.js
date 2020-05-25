de42e8a5ac5c25417057c5e5f3c17eee6aae5682
collero7 <collerortiz@gmail.com>
2020-05-25 20:02:06 +0200

    Hasta clase 24

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import ConditionalSection from './sections/conditional'
 
 class App extends Component {
   render(){
     return (
       <div className="App">
-        
+        <ConditionalSection />
       </div>
     );
   }
