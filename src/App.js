338eec078ca1cd364b75fac4799c03d474daee6f
collero7 <collerortiz@gmail.com>
2020-05-26 20:38:26 +0200

    Hasta clase 33

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+
+  constructor(){
+    super()
+    this.state = { mouseX: 0, mouseY: 0 }
+  }const
+
+  handleMouseMove = (e) => {
+    const { clientX, clientY } = e
+    this.setState({ mouseX: clientX, mouseY: clientY })
+  }
+
+  handleClick(e){
+    console.log(e);
+    console.log(e.nativeEvent);
+    alert('Hi there!');
+  }
+
   render(){
     return (
       <div className="App">
-        
+        <h4>Eventos</h4>
+        <button onClick={this.handleClick}>Hi there!</button>
+        <div onMouseMove={this.handleMouseMove} style={{ border: '1px solid #000', marginTop: 10, padding: 20, width: '5rem'}}>
+          <p>{this.state.mouseX}, {this.state.mouseY}</p>
+        </div>
       </div>
     );
   }
