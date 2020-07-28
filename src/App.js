9a12eb24ec6e9f9ce8be816cdadbe5ad15eecedf
collero7 <collerortiz@gmail.com>
2020-07-28 19:07:43 +0200

    Clase 45: El constructor

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+
+  //Constructor por defecto (se encuentra en el contexto)
+  /*
+  constructor (...args){
+    super(...args)
+  }
+  */
+
+  //Constructor modificado
+  constructor (props){
+    super(props)
+    this.bandera = false;
+    this.state = { mensajeInicial: 'mensaje inicial '}
+  }
+
+  //Funcion que cambia el texto del estado mensajeInicial cuando se pulsa el boton
+  handleClick = () => {
+    let mensaje1 = 'mensaje inicial ';
+    let mensaje2 = 'mensaje cambiado ';
+    this.setState( this.bandera ? { mensajeInicial: mensaje1 } : { mensajeInicial: mensaje2 })
+    this.bandera = !this.bandera;
+  }
+
   render(){
     return (
       <div className="App">
-        
+        <h4>Ciclo de montaje: constructor </h4>
+        { this.state.mensajeInicial }
+        <button onClick = { this.handleClick }> Cambiar mensaje </button>
       </div>
     );
   }
